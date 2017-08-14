6befbd9d75bc0afef2541dad113c2002681c5e5e
Sarah <sarahkane682@yahoo.com>
2017-08-13 22:46:43 -0700

    model tests

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -5,3 +5,33 @@ class TaskTest < ActiveSupport::TestCase
     assert true
   end
 end
+
+
+
+class TaskTest < ActiveSupport::TestCase
+  test "completed ture" do
+    task = Task.new(name: 'Bugs', completed: true , detail: 'FunTimesAndGO', deadline: Date.today  )
+    assert_equal true, task.completed
+  end
+
+  test "completed false" do
+    task = Task.new(name: 'Bugs', completed: false , detail: 'FunTimesAndGO', deadline: Date.today )
+    assert_equal false, task.completed
+  end
+
+  test "test  name" do
+    task = Task.new(name: 'Bugs', completed: false , detail: 'FunTimesAndGO', deadline: Date.today  )
+    assert_equal "Bugs", task.name
+  end
+
+  test "test detail" do
+    task = Task.new(name: 'Bugs', completed: false , detail: 'FunTimesAndGO', deadline: Date.today  )
+    assert_equal "FunTimesAndGO", task.detail
+  end
+
+  test "test deadline" do
+    task = Task.new(name: 'Bugs', completed: false , detail: 'FunTimesAndGO', deadline: Date.today )
+    assert_equal Date.today , task.deadline
+  end
+
+end
